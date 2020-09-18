cd23b59412770372be62d581328e7a57e2a57bf4
Zachary Darneille <zakdarn@gmail.com>
2020-09-18 02:02:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,11 +24,9 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 2
+  shipping_manifest
   # add 2 muskets to the shipping_manifest hash below
 
 
@@ -38,5 +36,6 @@ def adding
 
 
   # return the shipping_manifest hash below
+
 
 end
